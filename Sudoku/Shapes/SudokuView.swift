65c7de59beb4d795cd57282515f912eda9197604
Igor <pokerman154@gmail.com>
2020-05-14 20:16:58 +0300

    +

--- Sudoku/Shapes/SudokuView.swift
+++ Sudoku/Shapes/SudokuView.swift
@@ -9,16 +9,16 @@ class SudokuView: UIView {
     override func draw(_ rect: CGRect) {
         drawBasedAt(rect)
         if type != .shape { drawGridAt(rect) }
-//        switch type {
-//        case .diagonal: layer.addDiagonalsAt(rect, lineWidth: 1)
-//        case .twoDiagonals: layer.addTwoDiagonalsAt(rect, lineWidth: 1)
-//        case .romb: layer.addRombAt(rect, lineWidth: 1)
-//        case .hexagon: layer.addHexagonAt(rect, lineWidth: 1)
-//        case .shape:
-//            UIColor.dynamicBlack.setStroke()
-//            Shapes.getPathBasedAt(id, rect: rect,width: 2.5).stroke()
-//        default:break
-//        }
+        switch type {
+        case .diagonal: layer.addDiagonalsAt(rect, lineWidth: 1)
+        case .twoDiagonals: layer.addTwoDiagonalsAt(rect, lineWidth: 1)
+        case .romb: layer.addRombAt(rect, lineWidth: 1)
+        case .hexagon: layer.addHexagonAt(rect, lineWidth: 1)
+        case .shape:
+            UIColor.dynamicBlack.setStroke()
+            Shapes.getPathBasedAt(id, rect: rect,width: 2.5).stroke()
+        default:break
+        }
     }
 
     private func drawGridAt(_ rect:CGRect) {
